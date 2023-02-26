c3a98a71441aac46a0e577558026d74cbba1a5c7
Anant <anantgargin@gmail.com>
2023-02-26 11:21:08 +0530

    Second Commit

--- frontend/src/pages/SignUp.jsx
+++ frontend/src/pages/SignUp.jsx
@@ -2,5 +2,37 @@ import { useState } from 'react';
 import { Alert, Card, Col, Container, Row, Spinner } from "react-bootstrap";
 
 const SignUp = (props) => {
-    
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(null);
+    const dispatch = useAuthDispatch();
+    const handleSubmit = (ev, form) => {
+        ev.preventDefault();
+        setLoading(true);
+        signup(form)
+        .then((res) => {
+            console.log(res);
+            return login(dispatch, {username: form.username, password: form.password});
+        }).then((result) => {
+            console.log(result);
+            props.history.push('/home');
+        })
+        .catch((err) => console.error(err))
+        .finally(() => setLoading(false));
+    };
+
+    return(
+        <Container>
+            <Row>
+                <Col sm={10} md={8} lg={6} style={{margin:"auto"}}>
+                    <Card style={{padding:"10px"}}>
+                        <SignUpForm handleSubmit={handleSubmit}/>
+                        {loading && <div className="d-flex justify-content-center mt-2">
+                            <Spinner className="" animation="border" variant="primary" /> 
+                        </div>}
+                        {error && <Alert className="mt-2" show variant="danger">{error}</Alert>}
+                    </Card>
+                </Col>
+            </Row>
+        </Container>
+    )
 };
