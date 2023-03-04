649f2b51e267de1cd4e7e7b625c1ecb08d56e64d
A-Garg123 <78873371+A-Garg123@users.noreply.github.com>
2023-03-04 23:18:15 +0530

    Delete post.jsx

--- post.jsx
+++
@@ -1,8 +0,0 @@
-import { useState } from 'react';
-import { Button, Card, DropdownButton, Dropdown, Image, Modal } from 'react-bootstrap';
-import { useAuthState } from '../context/context';
-import { ApiService } from '../services/ApiService';
-import ConfirmDeleteModal from './ConfirmDeleteModal';
-import NewReply from './NewReply';
-
-const API_URL = process.env.REACT_APP_API_URL;
